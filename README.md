808a5c18e5cd326dca67f34dd219a63b33204ab6
edu.schaefer <94129764+SHEFEeee@users.noreply.github.com>
2024-02-14 14:09:35 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone de https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <h1>Oie pexoa, te amo S2....</h1>
+function Home() {
+  return <h1>Oie pexoa, te amo S2....</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
+}
 export default Home;
